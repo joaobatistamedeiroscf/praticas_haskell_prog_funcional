244166cf940ca0ff4db403a5151e2083359bbd7b
joaobatistamedeiroscf <joaobatistamedeiroscf@gmail.com>
2026-05-13 22:33:51 -0300

    add numEquallMax.hs

---
+++ Guardas/numEquallMax.hs
@@ -0,0 +1,21 @@
+
+
+maxthree::Int-> Int ->Int -> Int 
+numequalMax::Int -> Int -> Int -> Int 
+maxthree a b c 
+    |a >= b  && a >= c = a  
+    |b >= a && b >= c = b 
+    |c >= a && c >= b = c 
+
+numequalMax a b c
+    |a  ==  maxthree a b c && b == maxthree a b c && c == maxthree a b c = 3   
+    |a == maxthree a b c && b == maxthree a b c  = 2 
+    |a == maxthree a b c && c == maxthree a b c = 2
+    |b == maxthree a b c && c == maxthree a b c = 2 
+    |otherwise = 1
+
+
+
+main :: IO ()
+main =  do
+    print( numequalMax 8 8 9)
