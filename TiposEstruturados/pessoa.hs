7d21944d343c030c900865c7e26bbde7f67d5ac9
joaobatistamedeiroscf <joaobatistamedeiroscf@gmail.com>
2026-06-02 21:41:38 -0300

    update pessoa.hs

--- TiposEstruturados/pessoa.hs
+++ TiposEstruturados/pessoa.hs
@@ -1,7 +1,13 @@
 type Pessoa = (String , Int )
+
+
 idadePessoa :: Pessoa -> Int 
 idadePessoa (name , idade) = idade
-    
+
+namePessoa :: Pessoa -> String
+namePessoa (name,idade) = name
+
 main :: IO()
 main = do 
     print(idadePessoa("João" ,20))
+    print(namePessoa("Maria" ,20))
